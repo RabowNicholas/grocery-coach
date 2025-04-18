9234fe651a3836c901be51a498bcbfefa0f5fdb9
RabowNicholas <rabownick123@gmail.com>
2025-04-18 13:04:03 -0600

    chat bubble icon positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,7 +93,9 @@ export default function Home() {
           trackEvent("chat_button_clicked");
           window.open("https://tally.so/r/mVxOgy", "_blank");
         }}
-        className="fixed bottom-18 right-4 z-50 bg-sage text-white rounded-full py-2 px-4 shadow-md hover:bg-sage-muted font-bold text-3xl"
+        className={`fixed ${
+          showStickyCTA ? "bottom-18" : "bottom-4"
+        } right-4 z-50 bg-sage text-white rounded-full py-2 px-4 shadow-md hover:bg-sage-muted font-bold text-3xl`}
       >
         ?
       </button>
