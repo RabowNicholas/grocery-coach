c274ef7771eb9bbeeec1578bf5de9fa452046f2f
RabowNicholas <rabownick123@gmail.com>
2025-04-22 16:15:18 -0600

    utm_content

--- src/app/rice-bowl-plan/page.tsx
+++ src/app/rice-bowl-plan/page.tsx
@@ -12,6 +12,7 @@ export default function RiceBowlLandingPage() {
       utm_source: params.get("utm_source"),
       utm_medium: params.get("utm_medium"),
       utm_campaign: params.get("utm_campaign"),
+      utm_content: params.get("utm_content"),
       referrer: document.referrer,
     });
   }, []);
